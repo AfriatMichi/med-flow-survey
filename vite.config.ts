485d416a2338c97d72a531f4e81652da103e8f7e
AfriatMichi <michael.afriat@gmail.com>
2025-07-02 01:18:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// זה חשוב:
+export default defineConfig({
+  base: '/med-flow-survey/', // שים לב לשם הריפו!
+  plugins: [react()],
+})
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
