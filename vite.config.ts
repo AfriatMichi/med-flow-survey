2d2146ef5879b56e503a7db815b8abc79fafcb7a
AfriatMichi <michael.afriat@gmail.com>
2025-07-02 20:47:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 חובה כדי שזה יעבוד נכון בגיטהאב פייג׳ז
+  base: '/med-flow-survey/',
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
